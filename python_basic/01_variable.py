9f30907ae7c84254e8ba20a3992ae72373213790
TRax404 <tiryho22@gmail.com>
2026-07-09 22:54:23 +0600

    finish input and type convert

--- python_basic/01_variable.py
+++ python_basic/01_variable.py
@@ -28,7 +28,22 @@ print("Is Student:", is_student)
 name = "Trax"
 age = 10
 cGpa = 3.57
+_x = "ray"
+full_name = " G M Tirtho deb ray"
+
+is_Admin = True
+is_Admin = False
 
 print(name)
 print(age)
 print(cGpa)
+print(_x)
+print(full_name)
+print(is_Admin)
+print(type(is_Admin))
+
+print("GM", "Tirtho" ,"Deb", "Ray", sep="-")
+
+student_name = "Tarx"
+
+print("hello" , student_name)
---
+++ python_basic/02_input&type.py
@@ -0,0 +1,44 @@
+name = input("Enter your name ")
+print(name)
+
+# age = input(" Enter your age : ")
+# print(age)
+
+section = input("Section: ")
+
+print(type(section))
+
+
+# Convert String → Integer
+age =int(input("Enter your age "))
+print(age+12)
+
+# Float Conversion
+result = float(input("This is your result:"))
+print(result)
+
+pi = "3.1416"
+
+pi = float(pi)
+
+print(pi)
+
+
+
+name = input("Enter Name: ")
+
+age = int(input("Enter Age: "))
+
+department = input("Department: ")
+
+print()
+print("Student Information")
+print("----------------------")
+print("Name :", name)
+print("Age :", age)
+print("Department :", department)
+
+
+now_age = int(input("Enter your age :"))
+future_age = now_age+10
+print(future_age)
