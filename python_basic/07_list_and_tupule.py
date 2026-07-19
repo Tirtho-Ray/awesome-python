a3b4df31a8eab53cc18d593cd600d987f8223c04
TRax404 <tiryho22@gmail.com>
2026-07-19 10:52:18 +0600

    compalete list

--- python_basic/07_list_and_tupule.py
+++ python_basic/07_list_and_tupule.py
@@ -2,3 +2,19 @@
 # mark3 =66
 
 mark =[50,60,70,20,40]
+# print(mark[0:4])
+
+# mark.append(45)
+mark.sort() # shot to small to big
+
+mark.sort(reverse=True) # big to small 
+
+mark.reverse() # its total reverse 
+
+mark.insert(4,100)# here in list where need push element  -> first is index number and second is what value need insert 
+
+# mark.remove(3)
+
+mark.pop(2) # remove any elements form list of index
+
+print(mark)
