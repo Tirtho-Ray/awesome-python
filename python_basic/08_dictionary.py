4d1f14bbff0f2af625ae816c39b04ec90c07bdd5
TRax404 <tiryho22@gmail.com>
2026-08-24 21:21:45 +0600

    diractory start

---
+++ python_basic/08_dictionary.py
@@ -0,0 +1,24 @@
+# dictionary 
+"""
+info ={
+    "name":"Trax",
+    "role":"admin",
+    "age": 50,
+    "isAdult": True,
+    "subjects": ["ptyhon","js","ts","nginx"]
+
+}
+
+info["age"]
+print(info)
+"""
+
+# Nested
+
+student={
+    "name":"Trax",
+    "subject":{
+        "physics":90,
+        "chemistry": 80
+    }
+}
